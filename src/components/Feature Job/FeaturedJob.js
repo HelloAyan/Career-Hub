d55acfa7eca55c1457043aec20f62f087ce7c2a7
HelloAyan <helloayan171@gmail.com>
2023-04-12 23:54:06 +0600

    Completed fetch data for featured part

--- src/components/Feature Job/FeaturedJob.js
+++ src/components/Feature Job/FeaturedJob.js
@@ -1,28 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Styles from '../Feature Job/FeaturedJob.module.css';
 import googleImg from '../../assets/All Images/google-1-1 1.png';
 
 const FeaturedJob = () => {
+    const [featured, setFeatured] = useState([]);
+
+    useEffect(() => {
+        fetch('featured.json')
+            .then(res => res.json())
+            .then(data => setFeatured(data));
+    }, [])
     return (
         <div>
             <div className={Styles.featuredTitle}>Featured Jobs</div>
             <div className={Styles.featureContent}>Explore thousands of job opportunities with all the information you need. Its your future</div>
             <div className={Styles.featuredJobBodyContainer}>
-                <div className={Styles.jobPostSection}>
-                    <div> <img src={googleImg} alt="" /> </div>
-                    <div className={Styles.postTitle}>Technical Database Engineer</div>
-                    <div className={Styles.companyName}>Google LLC</div>
-                    <div className={Styles.remoteFullTimeSection}>
-                        <div className={Styles.jobTypeSection}>Remote</div>
-                        <div className={Styles.jobTypeSection}>Full Time</div>
-                    </div>
-                    <div className={Styles.locationSalarySection}>
-                        <div> <i class="fa-solid fa-location-dot"></i> Dhaka, Bangladesh</div>
-                        <div> <i class="fa-solid fa-dollar-sign"></i> Salary : 100K - 150K</div>
-                    </div>
-                    <div className={Styles.viewBtn}> <button>View Details </button> </div>
-                </div>
-                <div className={Styles.jobPostSection}>Microsoft</div>
+                {featured.map(v => (
+                    <>
+                        <div className={Styles.jobPostSection}>
+                            <div> <img src={googleImg} alt="" /> </div>
+                            <div className={Styles.postTitle}> {v.JobTitle}</div>
+                            <div className={Styles.companyName}>{v.CompanyName}</div>
+                            <div className={Styles.remoteFullTimeSection}>
+                                <div className={Styles.jobTypeSection}>{v.RemoteOrOnsite} </div>
+                                <div className={Styles.jobTypeSection}>{v.FulltimePartTime}</div>
+                            </div>
+                            <div className={Styles.locationSalarySection}>
+                                <div> <i class="fa-solid fa-location-dot"></i> {v.Location}</div>
+                                <div> <i class="fa-solid fa-dollar-sign"></i> Salary : {v.Salary}</div>
+                            </div>
+                            <div className={Styles.viewBtn}> <button>View Details </button> </div>
+                        </div>
+                    </>
+                ))}
             </div>
             <div className={Styles.allJobBtn}> <button>See All Jobs </button> </div>
         </div>
--- src/components/Job Category/JobCategory.js
+++ src/components/Job Category/JobCategory.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Styles from '../Job Category/JobCategory.module.css';
 import accountImg from '../../assets/Icons/accounts 1.png';
 import creativeImg from '../../assets/Icons/business 1.png';
@@ -6,18 +6,32 @@ import marketingImg from '../../assets/Icons/social-media 1.png';
 import EngineeringImg from '../../assets/Icons/chip 1.png';
 
 const JobCategory = () => {
+
+    const [category, setCategory] = useState([]);
+
+    useEffect(() => {
+        fetch('category.json')
+            .then(res => res.json())
+            .then(data => setCategory(data));
+    }, [])
+
     return (
         <div className={Styles.jobCategoryContainer}>
             <div style={{ fontSize: '45px', color: 'black', textAlign: 'center' }} >Job Category List</div>
             <div style={{ fontSize: '16px', color: '#757575', textAlign: 'center', marginTop: '15px' }}>Explore thousands of job opportunities with all the information you need. Its your future</div>
 
             <div className={Styles.jobTitleSection}>
-                <div className={Styles.jobCategory}>
-                    <div className={Styles.imgSection} > <img src={accountImg} alt="" /> </div>
-                    <div className={Styles.postTitle}> Account & Finance</div>
-                    <div className={Styles.availableJob}> Job available</div>
-                </div>
-                <div className={Styles.jobCategory}>
+                {category.map(v => (
+                    <>
+                        <div className={Styles.jobCategory}>
+                            <div className={Styles.imgSection} > <img src={accountImg} alt={v.CategoryName} style={{ width: '40px', height: '40px' }} /> </div>
+                            <div className={Styles.postTitle}> {v.CategoryName}</div>
+                            <div className={Styles.availableJob}>{v.JobAvailable} Job available</div>
+                        </div>
+                    </>
+                ))}
+
+                {/* <div className={Styles.jobCategory}>
                     <div className={Styles.imgSection} > <img src={creativeImg} alt="" /> </div>
                     <div className={Styles.postTitle}> Creative Design </div>
                     <div className={Styles.availableJob}>Job available</div>
@@ -31,7 +45,7 @@ const JobCategory = () => {
                     <div className={Styles.imgSection} > <img src={EngineeringImg} alt="" /> </div>
                     <div className={Styles.postTitle}> Engineering Job </div>
                     <div className={Styles.availableJob}>Job Available</div>
-                </div>
+                </div> */}
             </div>
         </div>
     )
