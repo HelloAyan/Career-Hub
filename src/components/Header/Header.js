558dbe53e3c1dbb15b084dd5f6411ed3f69f01e1
HelloAyan <helloayan171@gmail.com>
2023-04-14 22:57:49 +0600

    Mobile responsive completed for home page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,9 +7,9 @@ const Header = () => {
         <div className={Styles.headerBody}>
             <div className={Styles.contentArea}>
                 <div className={Styles.contentAreaText}>
-                    <div style={{ marginTop: '100px', lineHeight: '75px' }}> <div>One Step</div>  Closer To Your <div style={{ color: '#8F7DFF' }}>Dream Job </div> </div>
+                    <div className={Styles.contentTitle}> <div>One Step</div>  Closer To Your <div style={{ color: '#8F7DFF' }}>Dream Job </div> </div>
                 </div>
-                <div style={{ marginTop: '25px', color: '#757575' }}>
+                <div className={Styles.contentBody}>
                     <div> Explore thousands of job opportunities with all the </div> <div> information you need. Its your future. Come find it. Manage all </div> <div> your job application from start to finish. </div>
                 </div>
                 <div className={Styles.getStartedBtn}> <button> Get Started</button> </div>
